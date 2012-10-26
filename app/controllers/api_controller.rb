391d1db44b4da25ecfb60c702acf502e9de6982d
Joseph Chow <kindofsleepy@ubuntu.(none)>
2012-10-26 16:04:45 -0600

    removed some stale code, started working on front end portion

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -9,16 +9,9 @@ class ApiController < ApplicationController
 	end	
 
 	def getToken
-		#rdio = Rdio.init("x8zxcv2b35bhmm4yj86xa3u6","Ey83GMjKBY")
 		json_string = RDIO_OBJ.call("getPlaybackToken", {"domain" => "localhost"})
-		
-
 		respond_with json_string
 		
-		#respond_to do |format|
-		#	format.json{render :json=>json_string}
-	#	end
-
 	end
 
 	def findArtist
